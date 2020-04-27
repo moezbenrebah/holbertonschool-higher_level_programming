25d21014ec00c0faa59927efaff0bbc1a1140d94
Boul3ez85 <benrebah.moez@gmail.com>
2020-04-27 23:45:02 +0100

    code efficiency

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* check_cycle - check if the singly list contain a cycle or not
+* @list: pointer to a singly list
+*
+*Return: 1 if successed, otherwise 0
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *start, *new;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	start = new = list;
+	while (start && new && new->next)
+	{
+		start = start->next;
+		new = new->next->next;
+		if (start == new)
+			return (1);
+	}
+	return (0);
+}
