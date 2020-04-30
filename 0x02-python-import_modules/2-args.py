85e274b3c5bb93b5efc935ef8638fe474070113f
Boul3ez85 <benrebah.moez@gmail.com>
2020-04-30 22:51:28 +0100

    print number&list of argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+if __name__ == "__main__":
+    import sys
+    idx = len(sys.argv) - 1
+    print("{} {}{}".format(idx, "argument" if idx == 1 else "arguments",
+                           '.' if idx == 0 else ':'))
+    for i in range(idx):
+        print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
