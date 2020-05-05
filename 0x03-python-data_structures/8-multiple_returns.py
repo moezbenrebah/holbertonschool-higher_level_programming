aef6237c5807dade7e8a81d371d1c0375ceec2ad
Boul3ez85 <benrebah.moez@gmail.com>
2020-05-05 22:26:52 +0100

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for i in sentence:
-        if i == "":
-            return None
-        else:
-            return (len(sentence), sentence[0])
+    if len(sentence) > 0:
+        return (len(sentence), sentence[0])
+    else:
+        return None
