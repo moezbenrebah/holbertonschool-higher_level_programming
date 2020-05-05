e63a994e36f01785fe145a9b41ed957ea0c87b5f
Boul3ez85 <benrebah.moez@gmail.com>
2020-05-05 23:40:18 +0100

    delete item in nth index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if my_list:
+        if idx < 0:
+            return my_list
+        elif idx >= len(my_list):
+            return my_list
+        else:
+            del my_list[idx]
+    return my_list
