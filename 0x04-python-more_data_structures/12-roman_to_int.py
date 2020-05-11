768e6d4ee43e8bc47ebf37b20c6cb17054d4eea9
Boul3ez85 <benrebah.moez@gmail.com>
2020-05-11 02:34:51 +0100

    roman numerals to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    d_1 = {"M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100,
+           "XC": 90, "L": 50, "XL": 40, "X": 10, "V": 5, "IV": 4, "I": 1}
+    res = 0
+    prev = 0
+    for i in roman_string:
+        if d_1[i] > prev:
+            res += d_1[i] - 1 * prev
+        else:
+            res = d_1[i]
+    return res
