8663003e0c42c65de9924825958042199c2215e5
Boul3ez85 <benrebah.moez@gmail.com>
2020-05-18 22:39:45 +0100

    raise a message

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    try:
-        raise NameError
-    except NameError as error:
-        print(message, error)
+    raise NameError(message)
