248d097a2b9e952a833326547cf306726daf3002
Boul3ez85 <benrebah.moez@gmail.com>
2020-05-18 21:34:45 +0100

    safe division

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+def list_division(my_list_1, my_list_2, list_length):
+    l = []
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            print("out of range")
+            result = 0
+        except (TypeError, ValueError):
+            print("wrong type")
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        finally:
+            l.append(result)
+    return l
