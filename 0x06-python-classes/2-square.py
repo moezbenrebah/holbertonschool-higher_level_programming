9113f3a19fc4f2ad83895a6d4c254cce6edbda85
Boul3ez85 <benrebah.moez@gmail.com>
2020-05-19 19:09:48 +0100

    square_2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+                raise ValueError("size must be >= 0")
+        self.__size = size
