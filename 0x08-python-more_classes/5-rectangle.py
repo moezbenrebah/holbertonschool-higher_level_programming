aef132a48919fd49a21962200c8283210e816eb8
Boul3ez85 <benrebah.moez@gmail.com>
2020-05-25 19:35:10 +0100

    delete rectangle

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -116,4 +116,4 @@ class Rectangle:
         return 'Rectangle({}, {})'.format(self.width, self.height)
 
     def __del__(self):
-        print("Bye rectangle[...]")
+        print("Bye rectangle...")
