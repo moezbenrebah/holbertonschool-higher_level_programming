800e215212e1e70c8d2d70c2334763f7fd03cb24
Boul3ez85 <benrebah.moez@gmail.com>
2020-05-27 10:36:17 +0100

    adding operation fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -20,7 +20,7 @@ def add_integer(a, b=98):
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    elif type(b) not in [int, float]:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    else:
-        return int(a) + int(b)
+
+    return int(a) + int(b)
