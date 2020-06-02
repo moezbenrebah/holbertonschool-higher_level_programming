ee4427782b05e06d19a87fe300fb010339bfaf72
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-02 21:18:55 +0100

    inherited or not

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+"""Defines inherits_form() function"""
 
 
 def inherits_from(obj, a_class):
+    """
+    checks if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class
+
+    Arguments:
+        obj: object
+        a_class: class object
+
+    Returns:
+        Bool
+    """
+
     if type(obj) is not a_class and issubclass(type(obj), a_class):
         return True
     else:
