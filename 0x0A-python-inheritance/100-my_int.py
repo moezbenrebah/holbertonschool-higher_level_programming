b860d956f3281206081f96524665aa344fc9b241
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-02 23:39:08 +0100

    override

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,8 +2,11 @@
 """Module for MyInt class."""
 
 
-
 class MyInt(int):
+    """
+    Reprensting a subclass of int class.
+    """
+
     def __equal__(self, x):
         """override and inverte equal."""
         return int(self) != int(x)
