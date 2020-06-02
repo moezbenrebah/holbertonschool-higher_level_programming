b528ffa88dbbe980f39a83fa9aaf89ef279d0ec2
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-02 20:39:44 +0100

    sorted list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-'''Defines: class MyList'''
+"""Defines: class MyList"""
 
 
 class MyList(list):
 
     def __init__(self):
-        '''
+        """
         initializes list
 
         Arguments: None
         Returns: None
-        '''
+        """
         super().__init__
 
     def print_sorted(self):
-        '''
+        """
         prints a sorted list
 
         Arguments:
             None
+
         Returns:
             None
-        '''
+        """
 
         print(sorted(self))
