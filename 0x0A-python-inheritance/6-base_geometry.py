f8aa81392199f679adde3abdde0ddbb63399369b
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-02 21:28:43 +0100

    raise Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """Methode to calculate area."""
+        raise Exception("area() is not implemented")
