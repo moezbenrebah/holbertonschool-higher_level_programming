9b1424f0577142e292c1a409e1842a9eddab5b6a
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-02 12:05:48 +0100

    available attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def lookup(obj):
+    '''the list of availables attributes and methods of an object'''
+
+    '''
+    Arguments:
+        obj: object
+
+    Returns: list
+    '''
+    return dir(obj)
