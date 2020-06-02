595c78f596a3f5439498c5bf4dfe8a4a7eab63a5
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-02 21:14:45 +0100

    kind of class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""Defines is_kind_of_class() function"""
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    checks if the object is an instance
+
+    Arguments:
+        object: obj
+        object class: object class
+
+    Returns:
+        Bool
+    """
+
     if type(obj) is a_class:
         return True
     if issubclass(type(obj), a_class):
