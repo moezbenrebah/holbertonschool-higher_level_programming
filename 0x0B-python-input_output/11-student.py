ac9a0511fefd2dcf79c318b4361876e90d43ac0b
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-03 22:15:01 +0100

    student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,13 +4,13 @@ import json
 
 
 class Student:
-    """class for student"""
+    """class for student."""
     def __init__(self, first_name, last_name, age):
-        """ initialization"""
+        """initialization."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """retrieve a dictionary"""
+        """retrieves a dictionary."""
         return self.__dict__
