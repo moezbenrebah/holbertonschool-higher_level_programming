38bec8e9d3fe1f3fb3fbd3b7a1382837a930e056
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-03 23:03:17 +0100

    student class

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for Studen class"""
+
+
+class Student:
+    """Class for student"""
+    def __init__(self, first_name, last_name, age):
+        """Constructor."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        dictionary = {}
+        for attr in attrs:
+            try:
+                dictionary[attr] = self.__dict__[attr]
+            except Exception:
+                pass
+        return dictionary
