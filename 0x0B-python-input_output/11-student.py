1c462e1a86bd21b9b9051092fe2e43a035c52c5a
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-03 22:10:23 +0100

    student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -8,19 +8,11 @@ import json
 class Student:
 
     def __init__(self, first_name, last_name, age):
+        """ initialization"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    """
-    Represents:
-        a class Student that defines a student.
-    Attributes:
-        first_name (string): the student first name.
-        last_name (string): the student last name.
-        age (integer): student age.
-    Returns:
-        simple data structure.
-    """
 
     def to_json(self):
+        """retrieve a dictionary"""
         return self.__dict__
