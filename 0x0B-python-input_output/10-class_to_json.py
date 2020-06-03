d7d6c77c37f997aade3412c4b638d7e59efab9c9
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-03 21:44:47 +0100

    dictionary description

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """
+    Represents:
+        a function that returns the dictionary description with simple
+        data structure (list, dictionary, string, integer and boolean)
+        for JSON serialization of an object.
+    Arguments:
+        obj (object): dictionary
+    Returns:
+        simple data structure
+    """
+
+    return obj.__dic__
