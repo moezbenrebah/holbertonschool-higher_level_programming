31cbada3537d42341d2e90391821f853b89b190c
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-04 10:28:04 +0100

    pascal triangle

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
+"""Module for pascal triangle function"""
+
 
 def pascal_triangle(n):
-    list = [[] for i in range(n)]
-    for i in range(n):
-        for j in range(i + 1):
-            if(j < i):
-                if(j == 0):
+    """
+    Represents:
+         function that returns a list of lists of integers
+         representing the Pascal’s triangle.
+    Argument:
+         n (integer): integer will be fill in list.
+    Returns:
+         list.
+    """
+    if n <= 0:
+        return []
+    else:
+        list = [[] for i in range(n)]
+        for i in range(n):
+            for j in range(i + 1):
+                if(j < i):
+                    if(j == 0):
+                        list[i].append(1)
+                    else:
+                        list[i].append(list[i - 1][j] + list[i - 1][j - 1])
+                elif(j == i):
                     list[i].append(1)
-                else:
-                    list[i].append(list[i - 1][j] + list[i - 1][j - 1])
-            elif(j == i):
-                list[i].append(1)
-    return list
+        return list
