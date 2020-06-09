ec15eea0a3198db7ffee1bece18e752842cf84f5
Boul3ez85 <benrebah.moez@gmail.com>
2020-06-09 14:03:55 +0100

    almost circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 #!usr/bin/python3
-""" Module for Base class."""
+"""Base module."""
 import json
 
 
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for square class."""
+"""Square module."""
 from models.rectangle import Rectangle
 
 
