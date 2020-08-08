a9b81f9cabc9a261fc1252b25324a8b3cdf5e9d7
Boul3ez85 <https://github.com/Boul3ez85>
2020-08-09 00:47:16 +0100

    fix code

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,20 +6,20 @@ But this time, write one that is safe from MySQL injections!
 """
 
 import MySQLdb
-from sys import argv
+import sys
 
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                         user=argv[1], passwd=argv[2], db=argv[3])
-
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states
-                WHERE name = '{}'
-                ORDER BY states.id ASC""".format(argv[4]))
+if __name__ == '__main__':
+    argument = sys.argv[4]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""SELECT cities.name
+                FROM cities JOIN states
+                ON cities.state_id = states.id
+                WHERE states.name = %s
+                ORDER BY cities.id ASC;""", (argument,))
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    print(", ".join([row[0] for row in rows]))
 
     cur.close()
     db.close()
