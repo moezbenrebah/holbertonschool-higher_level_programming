232abaccda0fc3c56cad1807d51a7717ab55e77b
Boul3ez85 <https://github.com/Boul3ez85>
2020-08-08 18:02:00 +0100

    fix code

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name = '{}'
+    cur.execute("""SELECT * FROM states WHERE name LIKE '{}'
                 ORDER BY states.id ASC"""
                 .format(argv[4]))
     rows = cur.fetchall()
