675ecabbeb0f6b90bb7a55e0014b84e27472c677
Boul3ez85 <https://github.com/Boul3ez85>
2020-08-08 16:45:17 +0100

    fix code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states
-                WHERE name LIKE 'N%' ORDER BY states.id ASC""")
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
