86b38231ce3c2549ef2c36f972901a387fb14939
Boul3ez85 <https://github.com/Boul3ez85>
2020-08-08 17:19:40 +0100

    fix code

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,8 +14,9 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '" + argv[4] + "'
-                ORDER BY states.id ASC")
+    cur.execute("""SELECT * FROM states
+                WHERE name = '{}'
+                ORDER BY states.id ASC""".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
