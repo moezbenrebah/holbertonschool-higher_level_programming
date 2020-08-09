a8bb8f20940ce56aedad31559009f329c3abf602
Boul3ez85 <https://github.com/Boul3ez85>
2020-08-09 09:10:46 +0100

    fix pep8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,8 +11,8 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__name__":
     eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]), 
-            pool_pre_ping=True)
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
 
     S = sessionmaker(bind=eng)
     session = S()
