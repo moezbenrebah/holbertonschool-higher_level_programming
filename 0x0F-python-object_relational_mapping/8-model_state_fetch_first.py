047a78cf1f3a1dca55def10ac1d3e12ef805263a
Boul3ez85 <https://github.com/Boul3ez85>
2020-08-09 09:03:09 +0100

    print first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+a script that prints the first State object from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__name__":
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), 
+            pool_pre_ping=True)
+
+    S = sessionmaker(bind=eng)
+    session = Session()
+    State = session.query(State).order_by(State.id).first()
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
