848a6db7a9f4aaf7a4100f99d175770e43db4748
Boul3ez85 <https://github.com/Boul3ez85>
2020-08-09 09:38:06 +0100

    fix code

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """
-a script that prints the first State object from the database hbtn_0e_6_usa
+Lists all cities from the database
 """
 
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
 
-if __name__ == "__name__":
-    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True)
     Base.metadata.create_all(eng)
-
     S = sessionmaker(bind=eng)
     session = S()
-    state = session.query(State).order_by(State.id).first()
+    state = session.query(State).first()
     if state:
         print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing")
-
-    session.close()
