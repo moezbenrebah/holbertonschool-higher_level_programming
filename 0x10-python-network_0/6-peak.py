ecf4a86dede567e0f95ab62210dfc0d98ae12a7b
Boul3ez85 <https://github.com/Boul3ez85>
2020-08-18 23:06:44 +0100

    find peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
+    """Return a peak in a list of unsorted integers."""
+
     i = 0
     p = list_of_integers
 
