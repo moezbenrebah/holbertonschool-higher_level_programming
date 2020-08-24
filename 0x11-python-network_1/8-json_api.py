8939289f92ae5f502dcbae51629a3e384ad219d5
Boul3ez85 <benrebah.moez@gmail.com>
2020-08-24 17:03:46 +0100

    retrieve API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    if len(argv) == 1:
+        let = ""
+    else:
+        let = argv[1]
+    payload = {"q": let}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
