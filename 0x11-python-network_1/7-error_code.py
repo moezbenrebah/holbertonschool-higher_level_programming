c76b66add8aaf0decce28e1a26dcc8c8c9d392e2
Boul3ez85 <benrebah.moez@gmail.com>
2020-08-24 16:18:38 +0100

    Error code with requests module

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     url = argv[1]
     res = requests.get(url)
     if res.status_code >= 400:
-        print(f"Error code: {res.status_code}")
+        print("Error code: {}".format(res.status_code))
     else:
         print(res.text)
