8c1bbdba17e78ce6445d2bc1da694293a10ed89c
Boul3ez85 <benrebah.moez@gmail.com>
2020-08-24 16:12:58 +0100

    Error code with requests module

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,11 +6,12 @@ the body of the response (decoded in utf-8).
 
 from sys import argv
 import requests
+from requests.exceptions import HTTPError
 
 if __name__ == "__main__":
     url = argv[1]
     try:
         res = requests.get(url)
         print(res.text)
-    except requests.RequestException as e:
-        print("Error code: {}".format(e))
+    except HTTPError as e:
+        print(f"Error code: {e}")
