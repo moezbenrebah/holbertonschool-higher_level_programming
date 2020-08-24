27db7d462080b83b79f9294bc6b6c0d7582e04ce
Boul3ez85 <benrebah.moez@gmail.com>
2020-08-24 17:08:21 +0100

    retrieve API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,8 +17,9 @@ if __name__ == "__main__":
 
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json()
-        if response == {}:
+        r_dict = r.json()
+        id, name = r_dict.get('id'), r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
             print("No result")
         else:
             print("[{}] {}".format(response.get("id"), response.get("name")))
