53af985d49b9528002cc6613a3665d778b7b7aab
Boul3ez85 <benrebah.moez@gmail.com>
2020-08-24 16:04:21 +0100

    Error code with requests module

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        res = requests.post(url)
+        print(res.text)
+    except requests.RequestException as e:
+        print("Error code: {}".format(e))
