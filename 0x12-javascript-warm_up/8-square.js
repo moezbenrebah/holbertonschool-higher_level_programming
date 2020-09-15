5d4a72c58bd882d5c9da3051f7d3e591ea40591d
Boul3ez85 <https://github.com/Boul3ez85>
2020-09-15 17:10:11 +0100

    print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const size = process.argv[2];
 
 if (isNaN(parseInt(size, 10))) {
-  console.log('Missing number of occurences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
