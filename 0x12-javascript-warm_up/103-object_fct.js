5aff51234d317286adfbd6196282622c68dcf5d7
Boul3ez85 <https://github.com/Boul3ez85>
2020-09-17 07:58:21 +0100

    object increment

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+'use strict';
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  myObject.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
