c1710d3cd3e1085da46b56787d01797fecf6b33e
Boul3ez85 <https://github.com/Boul3ez85>
2020-09-21 19:02:10 +0100

    fix module

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,8 +2,7 @@
 
 'use strict';
 
-const Rectangle = require('./4-rectangle.js');
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends require('./4-rectangle.js') {
   constructor (size) {
     super(size, size);
   }
