5cf0fe771659fd0a2f6a57696420a01439d1c0d9
Boul3ez85 <https://github.com/Boul3ez85>
2020-09-22 15:05:17 +0100

    read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,6 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, content) {
-  if (err) {
-    return console.error(err);
-  }
-  console.log(err || content);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
